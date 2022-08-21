2d2df0f8b7be3d98111fef1a3b176739271241aa
Tom Blake <tom.wanless@icloud.com>
2022-08-21 10:21:33 +0100

    Clean

--- temp/index.js
+++
@@ -1,7 +0,0 @@
-import { startBrowser } from "./tutorialFiles/browser.js";
-import PageScraper from "../pageScraper.js";
-
-// Start browser instance
-let browserInstance = startBrowser();
-
-export default () => PageScraper(browserInstance);
--- temp/tutorialFiles/_index.js
+++
@@ -1,7 +0,0 @@
-import browserObject from './browser.js';
-import scraperController from './_pageController.js';
-
-// Start browser instance
-let browserInstance = browserObject.startBrowser();
-
-scraperController(browserInstance);
